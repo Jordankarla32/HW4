49d6b83025addf40383940ec5dfab2f8408b543d
Jordan Karla <jordan.karla@kellogg.northwestern.edu>
2025-03-14 00:25:54 +0000

    save1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
-class ApplicationController < ActionController::Base
-  before_action :current_user
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
-  def current_user
-    puts "------------------ code before every request ------------------"
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id  # Log in the user after signup
+      redirect_to "/", notice: "Successfully signed up!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
