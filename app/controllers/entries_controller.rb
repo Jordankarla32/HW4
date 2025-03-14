abd8cd41f1b46849e4cb92994ad52c6e05c9d222
Jordan Karla <jordan.karla@kellogg.northwestern.edu>
2025-03-14 01:37:13 +0000

    think im done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,16 +5,20 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["place_id"] = params["place_id"]
-    
+    @entry = Entry.new(entry_params)
+    @entry.place_id = params[:place_id]
+
     if @entry.save
-      redirect_to place_path(@entry["place_id"]), notice: "Entry added successfully!"
+      redirect_to place_path(@entry.place_id), notice: "Entry added successfully!"
     else
       flash[:alert] = "Error: Could not save entry."
       render :new
     end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :description, :occurred_on, :image)
   end
 end
---
+++ db/migrate/20250314012627_add_fields_to_entries.rb
@@ -0,0 +1,5 @@
+class AddFieldsToEntries < ActiveRecord::Migration[7.0]
+  def change
+    add_column :entries, :image, :string
+  end
+end
